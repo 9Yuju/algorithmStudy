ae71a4c61f79b71cde6ecc556123f8323a1787af
ParkYuJu <98456463+9Yuju@users.noreply.github.com>
2022-04-18 17:15:47 +0900

    Add files via upload

---
+++ S3_2784_가로세로퍼즐.java
@@ -0,0 +1,23 @@
+package study;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class S3_2784_가로세로퍼즐 { //모르겠는데?
+
+	public static void main(String[] args) {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
